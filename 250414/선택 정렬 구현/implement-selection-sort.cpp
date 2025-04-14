a0b52c8277f4b631eb1a1eb6eedd36383ba9827e
pixxy <s0n.0f.a.g00gle@gmail.com>
2025-04-14 13:27:58 +0900

    Title: 선택 정렬 구현, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250414/선택 정렬 구현/implement-selection-sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int arrsize;
+int arr[100];
+
+int main() {
+    cin >> arrsize;
+    for (int i = 0; i < arrsize; i++) cin >> arr[i];
+
+    for (int i = 0; i < arrsize; i++)
+    {
+        int minindex = 0;
+        int min = 100;
+        for (int ii = i; ii < arrsize; ii++)
+        {
+            if (min > arr[ii])
+            {
+                min = arr[ii];
+                minindex = ii;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minindex];
+        arr[minindex] = temp;
+    }
+
+    for (int i = 0; i < arrsize; i++) cout << arr[i] << " ";
+
+    return 0;
+}
