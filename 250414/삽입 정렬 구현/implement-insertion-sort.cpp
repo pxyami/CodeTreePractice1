9c0d3637671e475dc0c3827ad8e298103ab09d5a
pixxy <s0n.0f.a.g00gle@gmail.com>
2025-04-14 14:47:33 +0900

    Title: 삽입 정렬 구현, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250414/삽입 정렬 구현/implement-insertion-sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arrsize = 0;
+	int arr[100];
+
+	cin >> arrsize;
+	for (int i = 0; i < arrsize; i++) cin >> arr[i];
+
+	for (int i = 0; i < arrsize - 1; i++)
+	{
+		if (arr[i] < arr[i + 1])
+		{
+			int ii = i;
+			do
+			{
+				int temp = arr[ii - 1];
+				arr[ii - 1] = arr[ii];
+				arr[ii] = temp;
+				ii--;
+			} while (ii - 1 >= 0 && arr[ii - 1] > arr[ii]);
+		}
+	}
+
+	for (int i = 0; i < arrsize; i++) cout << arr[i] << " ";
+
+	return 0;
+}
